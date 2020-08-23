97f6401e808f0eac196b6949ed4c08f768727d94
Lemo <lemo1234@gmail.com>
2020-08-23 16:37:57 -0700

    Improved setup.py
    Contain all the setuptools artifacts into a single top level subdirectory

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 import os
 import subprocess
 import sysconfig
+import shutil
 
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
@@ -34,34 +35,61 @@ class QmakeBuild(build_ext):
 
     def build_extension(self, ext):
         print(f'Building {ext.name} ...')
-        print('  (see .build/build.log for build details)')
-
-        build_dir = os.path.abspath('.build')
+        print('  (see build.log for build details)')
 
         # environment variables used during the build
         build_env = os.environ.copy()
         build_env['PYTHON_INCLUDE'] = sysconfig.get_path('include')
 
-        # create build directory
-        if not os.path.exists(build_dir):
-            os.makedirs(build_dir)
+        build_log = open('build.log', 'w')
 
-        build_log = open(os.path.join(build_dir, 'build.log'), 'w')
+        # make sure the build temp directory is created
+        os.makedirs(self.build_temp, exist_ok=True)
 
         # invoke qmake
+        project_file = os.path.join(darwin_root, 'darwin.pro')
         config = 'debug' if self.debug else 'release'
         subprocess.check_call(
-            [self.qmake_bin, '../darwin.pro', f'CONFIG+={config}'],
-            cwd=build_dir,
+            [self.qmake_bin, project_file, f'CONFIG+={config}'],
+            cwd=self.build_temp,
             env=build_env,
             stdout=build_log)
 
         # invoke make (this will likely take a while)
         subprocess.check_call(
             ['make', f'-j{os.cpu_count()}'],
-            cwd=build_dir,
+            cwd=self.build_temp,
             env=build_env,
             stdout=build_log)
+
+        # make sure we built the extension binary
+        ext_binary = os.path.join(
+            self.build_temp, 'bindings', 'python', 'darwin.so')
+        if not os.path.isfile(ext_binary):
+            raise RuntimeError(f"'{ext_binary}' is missing, aborting.")
+
+        # finally, copy the extension files
+        ext_dst = self.get_ext_fullpath(ext.name)
+        os.makedirs(os.path.dirname(ext_dst), exist_ok=True)
+        shutil.copy(ext_binary, ext_dst)
+
+
+# Pre-setup: if we're running setup.py from the darwin root location,
+# create a sandbox subdirectory to contain all the fallout from setuptools
+# (ex. darwin.egg-info, dist, ...)
+#
+# Other than avoiding the top level litter, this makes it easy to
+# do a full cleanup by just deleting the .build directory
+#
+darwin_root = os.path.dirname(os.path.abspath(__file__))
+
+if os.path.abspath(os.getcwd()) == darwin_root:
+    # create the build directory for the binding extension
+    build_root = os.path.abspath('.build')
+    os.makedirs(build_root, exist_ok=True)
+    os.chdir(build_root)
+else:
+    build_root = os.getcwd()
 
 
 setup(
@@ -74,5 +102,4 @@ setup(
     license='Apache License 2.0',
     ext_modules=[QmakeExtension(name='darwin')],
     cmdclass=dict(build_ext=QmakeBuild),
-    options={'build': {'build_lib': '.build/bindings/python'}},
     zip_safe=False)
